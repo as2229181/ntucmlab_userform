425c6baac3cdbfa534e56ac79ebcd8ef59961943
as2229181 <as2229181@gmail.com>
2023-06-17 15:40:12 +0800

    update_requirements

--- form/utils.py
+++ form/utils.py
@@ -5,7 +5,7 @@ from win32com import client
 from django.template.loader import render_to_string
 from django.http import JsonResponse
 def convert_to_pdf(input_path, output_path):
-    excel = client.Dispatch("Excel.Application")#利用dispatch連接 microsoft 應用程式  這裡是連接到excel
+    excel = client.Dispatch("Excel.Application")  # 利用dispatch連接 microsoft 應用程式  這裡是連接到excel
     doc = excel.Workbooks.Open(input_path)
     doc.SaveAs(output_path, FileFormat=57)  # FileFormat = 57 是 PDF 格式
     doc.Close()
