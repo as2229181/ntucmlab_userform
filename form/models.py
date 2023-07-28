9671613325f1069dfea818648663f0c10d6b0fd1
as2229181 <as2229181@gmail.com>
2023-07-28 09:52:09 +0800

    debug

--- form/models.py
+++ form/models.py
@@ -70,16 +70,16 @@ class SC(models.Model):
         verbose_name_plural = '血液血清手開單'
     def save(self, *args, **kwargs):
         if not self.scid:
-            # 获取当前年份
+            # 獲取當前年份
             year = timezone.now().year
-            # 获取当前年份下已有的最大序号
+            # 獲取當前年份下已有的最大序號
             max_serial_number = Max_ID.objects.filter(SC_max__startswith=str(year)).values_list('SC_max', flat=True).order_by('-SC_max').first()
             #max_serial_number = QC.objects.filter(qcid__startswith=str(year)).values_list('qcid', flat=True).order_by('-qcid').first()
             if max_serial_number:
-                # 从最大序号中提取数字部分并加1
+                # 從最大序号中提取數字部分並加1
                 num = int(max_serial_number[6:]) + 1
             else:
-                # 如果该年份下没有序号，从001开始
+                # 如果該年份下没有序號，從001開始
                 num = 1
         
             self.scid = f"{year}SC{str(num).zfill(4)}"
